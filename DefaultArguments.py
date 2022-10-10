8c89cd9631c1cab5148c703036dc82e96bd09b29
Saiksha Desai <sd0040@srmist.edu.in>
2022-10-10 13:29:12 +0530

    Create DefaultArguments.py

---
+++ DefaultArguments.py
@@ -0,0 +1,35 @@
+class EvenStream(object):
+    def __init__(self):
+        self.current = 0
+
+    def get_next(self):
+        to_return = self.current
+        self.current += 2
+        return to_return
+
+class OddStream(object):
+    def __init__(self):
+        self.current = 1
+
+    def get_next(self):
+        to_return = self.current
+        self.current += 2
+        return to_return
+        
+# Default Arguments in Python - Hacker Rank Solution START ----->
+def print_from_stream(n, stream=None):
+    if stream is None:
+        stream = EvenStream()
+# Default Arguments in Python - Hacker Rank Solution END ----->
+    for _ in range(n):
+        print(stream.get_next())
+
+
+queries = int(input())
+for _ in range(queries):
+    stream_name, n = input().split()
+    n = int(n)
+    if stream_name == "even":
+        print_from_stream(n)
+    else:
+        print_from_stream(n, OddStream())
